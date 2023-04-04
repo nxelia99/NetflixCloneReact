a956a24cd0127bfe74628283e72b12c00e6fb09c
Nxelia <nxelia99@gmail.com>
2023-04-04 17:28:44 +0200

    new commit

--- src/Components/HomeScreen.js
+++ src/Components/HomeScreen.js
@@ -2,6 +2,8 @@ import React from 'react'
 import "../Components/CSS/HomeScreen.css";
 import Nav from "./Nav.js";
 import Banner from "./Banner.js";
+import Row from "./Row.js"
+import requests from '../Requests';
 
 function HomeScreen() {
   return (
@@ -15,6 +17,47 @@ function HomeScreen() {
 
         {/* Row */}
         
+        <Row 
+        title='NETFLIX ORIGINALS'
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+        />
+
+      <Row 
+        title='trending Now'
+        fetchUrl={requests.fetchTrending}
+
+        />
+      <Row 
+        title='Top Rated'
+        fetchUrl={requests.fetchTopRated}
+
+        />
+      <Row 
+        title='Action Movies'
+        fetchUrl={requests.fetchActionMovies}
+
+        />
+      <Row 
+        title='Comedy Movies'
+        fetchUrl={requests.fetchComedyMovies}
+
+        />
+      <Row 
+        title='Horror Movies'
+        fetchUrl={requests.fetchHorrorMovies}
+
+        />
+      <Row 
+        title='Romance Movies'
+        fetchUrl={requests.fetchRomanceMovies}
+
+        />  
+      <Row 
+        title='Documentaries'
+        fetchUrl={requests.fetchDocumentaries}
+
+        />          
     
     </div>
 
