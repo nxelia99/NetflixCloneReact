6ee1b3833e766bcd2ae41053cade44bb719c3795
Nxelia <nxelia99@gmail.com>
2023-04-07 18:02:09 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import HomeScreen from './Components/screens/HomeScreen.js';
 import {
@@ -7,11 +7,24 @@ import {
   Route
 } from "react-router-dom";
 import LoginScreen from './Components/screens/LoginScreen.js';
+import { auth } from './firebase';
 
 
 function App() {
 
   const user = null;
+
+  useEffect(() =>{
+    const unsubscribe = auth.onAuthStateChanged((userAuth) =>{
+      if (userAuth){
+        console.log(userAuth)
+      }else{
+
+      }
+    })
+
+    return unsubscribe;
+  }, [])
 
 
   return (
--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyDnFS5HNuUT0vZvKz9E1HhW9kzeUKUQKII",
   authDomain: "react-netflix-clone-ad653.firebaseapp.com",
@@ -17,5 +15,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
