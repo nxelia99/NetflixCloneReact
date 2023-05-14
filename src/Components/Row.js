83fc37f52d838e30f91069a1e28d6fc27babbc1c
Nxelia <nxelia99@gmail.com>
2023-05-14 12:47:51 +0200

    new commit

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -17,29 +17,39 @@ function Row({title, fetchUrl, isLargeRow = false}) {
 
     },[fetchUrl]);
 
-    console.log(movies)
 
   return (
+
     <div className='row'>
         <h2>{title}</h2>
 
-        
-
         <div className='row_posters'>
-        {movies.map((movie) =>(
-            ((isLargeRow && movie.poster_path) || (!isLargeRow && movie.backdrop_path)) && (
+            {movies.map((movie) =>(
+                ((isLargeRow && movie.poster_path) || (!isLargeRow && movie.backdrop_path)) && (
+                
+                <div>    
 
-            <img className={`row_poster ${isLargeRow && "row_posterLarge"}`}
-            key={movie.id}
-            src={`${base_url}${
-                isLargeRow ? movie.poster_path : movie.backdrop_path
-            }`} alt={movie.name} />
-            )))}
-            
+                <img className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+                key={movie.id}
+                src={`${base_url}${
+                    isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`} alt={movie.name} />
+
+                <div className='bottom_row'>
+                    <p>{movie?.title || movie?.name || movie?.original_name}</p> 
+                    <i>{BiPlay}</i>
+                </div>
+                </div>
+
+                )))}
+
+           
+             
+
+        </div>
 
     </div>
 
-    </div>
   )
 }
 
